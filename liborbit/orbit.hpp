2443e7fcf19ff83c9b0db56664c66d417fe1e6b0
Colleague Riley <50218871+ColleagueRiley@users.noreply.github.com>
2022-06-03 12:13:29 -0400

    Update orbit.hpp

--- liborbit/orbit.hpp
+++ liborbit/orbit.hpp
@@ -1,5 +1,10 @@
 #include <iostream>
-
 namespace orbit{
+	struct ellipse{int width,length; int x=0, y=0;};
+    ellipse getEllipse(double length, double foci1, double foci2);
+};
 
+orbit::ellipse orbit::getEllipse(double length, double foci1, double foci2){
+	double e=(double)((foci1-foci2))/length;
+    return {length, length * (double)(1-e) };
 }
